7a3111bb8ec571ff97e6e4dbd210a7aa81413c0d
Md Al Hossain Mukib <143283871+mdmokib@users.noreply.github.com>
2025-03-29 12:41:33 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+def greet() :
+    print("My name is Mukib")
+    print("I love Erina most")
+    print("But she does not care anything about me")
+
+greet()
+
+#function that allows input
+
+def story_of_mukib(name):
+    print(f"{name} hurts me everytime")
+    print(f"I will never ask {name} for love anymore")
+story_of_mukib("Erina")    

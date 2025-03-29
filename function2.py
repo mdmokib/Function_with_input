34ed0b12d8e8183b4877a3f09883bb72e4ffc88d
Md Al Hossain Mukib <143283871+mdmokib@users.noreply.github.com>
2025-03-29 12:51:56 +0600

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,5 @@
+#function with more than 1 input
+def mukib(name="Mukib", location="Dhaka"):
+    print(f"Hello {name}")
+    print(f"You are from {location}")
+mukib(location="Dhaka", name="Mukib")   
